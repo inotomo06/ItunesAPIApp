69c94982f9d1e0be089699414d8701812ca3c85b
inotomo06 <86100981+inotomo06@users.noreply.github.com>
2022-05-09 16:48:12 +0900

    Initial Commit

---
+++ testITunes/testITunesApp.swift
@@ -0,0 +1,17 @@
+//
+//  testITunesApp.swift
+//  testITunes
+//
+//  Created by cmStudent on 2022/05/09.
+//
+
+import SwiftUI
+
+@main
+struct testITunesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
