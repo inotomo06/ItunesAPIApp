49c8c3e7e7b3fc0b93c986103e868a69434e2a5e
inotomo06 <86100981+inotomo06@users.noreply.github.com>
2022-05-13 11:03:23 +0900

    Style：インデントを揃えた

--- testITunes/View/RegistrationView.swift
+++ testITunes/View/RegistrationView.swift
@@ -8,25 +8,24 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    // ひとつ前のviewに戻るよう
     @Environment(\.presentationMode) var presentation
-    
+    // 検索履歴用
     @Binding var binText: String
-    
+    // 履歴をためておく配列
     @State private var searchHistory: [String] = []
     
     var body: some View {
         
         VStack {
             HStack {
-                
                 Spacer()
-                
                 Button {
+                    // ひとつ前のView戻る
                     self.presentation.wrappedValue.dismiss()
                 } label: {
                     Text("戻る")
                 }
-                
                 Spacer()
                 Text("検索履歴")
                     .padding()
@@ -51,7 +50,6 @@ struct RegistrationView: View {
                 } label: {
                     Text("表示")
                 }
-                
                 Spacer()
             }
             
@@ -60,7 +58,6 @@ struct RegistrationView: View {
                     Text(books)
                 }
             }
-            
             Spacer()
         }
     }
